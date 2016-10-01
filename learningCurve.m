c91e63cbe6db1289509fd14b270c087e7d4716d4
Thierry Wambo <jean.oscar@yahoo.fr>
2016-10-01 12:06:27 -0500

    Update learningCurve.m

--- learningCurve.m
+++ learningCurve.m
@@ -17,43 +17,12 @@ function [error_train, error_val] = ...
 % Number of training examples
 m = size(X, 1);
 
-% You need to return these values correctly
+% Initializing some parameters
 error_train = zeros(m, 1);
 error_val   = zeros(m, 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the cross validation errors in error_val. 
-%               i.e., error_train(i) and 
-%               error_val(i) should give you the errors
-%               obtained after training on i examples.
-%
-% Note: You should evaluate the training error on the first i training
-%       examples (i.e., X(1:i, :) and y(1:i)).
-%
-%       For the cross-validation error, you should instead evaluate on
-%       the _entire_ cross validation set (Xval and yval).
-%
-% Note: If you are using your cost function (linearRegCostFunction)
-%       to compute the training and cross validation error, you should 
-%       call the function with the lambda argument set to 0. 
-%       Do note that you will still need to use lambda when running
-%       the training to obtain the theta parameters.
-%
-% Hint: You can loop over the examples with the following:
-%
-%       for i = 1:m
-%           % Compute train/cross validation errors using training examples 
-%           % X(1:i, :) and y(1:i), storing the result in 
-%           % error_train(i) and error_val(i)
-%           ....
-%           
-%       end
-%
+% ======================  CODE  ======================
 
-% ---------------------- Sample Solution ----------------------
-% 1-choose sample size 2-defined new size of theta, 3-compute new optimized theta
-%4- set lambda to 0, 5-apply costFunctionReg on training sample to compute   J_train=error_train.
    fprintf("\n ITERATING OVER THE NUMBER OF TRAINING EXAMPLES \n");
 for i=1:m
    %X=X(1:i,:);
